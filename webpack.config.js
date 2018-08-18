b8fd876916b9160c09e049bfc6a7290bf0cf7c32
Christiaan Goossens <contact@christiaangoossens.nl>
2018-08-18 10:57:15 +0200

    Added skeleton

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractSass = new ExtractTextPlugin({
+    filename: "./assets/css/[name].css"
+});
+
+module.exports = {
+	entry: {
+		main: [
+			'./assets/scss/main.scss',
+		]
+	},
+	output: {
+		filename: "./assets/js/[name].js"
+	},
+	module: {
+       rules: [{
+           test: /\.scss$/,
+           use: extractSass.extract({
+               use: [{
+                   loader: "css-loader"
+               }, {
+                   loader: "sass-loader",
+                   options: {
+                    }
+               }],
+               fallback: "style-loader"
+           })
+	   }, {
+           test: /^(?!.*(\.scss|\.css|\.js|\.vue))/,
+           use: [{
+               loader: "url-loader"
+           }]
+       }]
+	},
+	plugins: [
+		extractSass
+	]
+};
